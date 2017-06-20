5c41551f429fa78e9ca8581cb82d736330f6d047
Emma <emma.upton.95@gmail.com>
2017-06-20 20:08:12 +0100

    Adding todo's/improvements for solution to problem 1

--- haskell/eulers_problems/problem1.hs
+++ haskell/eulers_problems/problem1.hs
@@ -2,3 +2,5 @@
 --Find the sum of all the multiples of 3 or 5 below 1000.
 
 answer = sum(filter(\(x) -> (x `mod` 3==0) || (x `mod` 5==0))[1..999])
+--TODO: implement own sum and filter functions
+--TODO: Make dynamic function that takes arbritrary limit
