0c928a5eff75f7e2c585e4c880f7d9b195f9e364
Emma-Louise Upton <emma-louise.upton@hmrcaspire.com>
2017-07-12 15:19:44 +0100

    Solution for in-place string reversal

---
+++ python/interview_cake/problem26.py
@@ -0,0 +1,17 @@
+#Write a function to reverse a string in-place
+
+def inplaceStringReversal(string):
+    lst = list(string)
+    if(len(lst)%2 == 0):
+        limit = len(lst)/2
+    else:
+        limit = (len(lst) - 1)/ 2
+
+    for i in range(limit):
+        a = lst[i]
+        b = lst[-(i+1)]
+        print(b + a + str(i))
+        lst[i] = b
+        lst[-(i+1)] = a
+
+    print(lst)
